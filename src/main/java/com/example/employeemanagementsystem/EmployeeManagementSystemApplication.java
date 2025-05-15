20bb9de4afad8ec3efdec84d17d782a5bde8128e
huyldph <ledinhhuy26032004@gmail.com>
2025-05-15 10:31:37 +0700

    feat(config): load environment variables from .env file using Dotenv

--- src/main/java/com/example/employeemanagementsystem/EmployeeManagementSystemApplication.java
+++ src/main/java/com/example/employeemanagementsystem/EmployeeManagementSystemApplication.java
@@ -1,5 +1,6 @@
 package com.example.employeemanagementsystem;
 
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -7,6 +8,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EmployeeManagementSystemApplication {
 
     public static void main(String[] args) {
+        // Nạp file .env
+        Dotenv dotenv = Dotenv.load();
+
+        // Đẩy các biến vào System properties để Spring Boot có thể đọc được
+        System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+        System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+        System.setProperty("GOOGLE_CLIENT_ID", dotenv.get("GOOGLE_CLIENT_ID"));
+        System.setProperty("GOOGLE_CLIENT_SECRET", dotenv.get("GOOGLE_CLIENT_SECRET"));
+        System.setProperty("JWT_SIGNER_KEY", dotenv.get("JWT_SIGNER_KEY"));
+
         SpringApplication.run(EmployeeManagementSystemApplication.class, args);
     }
 
